90f6a59878d0f3899c42fc2c43bbd17c75e5d540
Guilherme Dytz dos Santos <guidytz@gmail.com>
2021-05-06 17:59:36 -0300

    Topic implementation

--- src/peer/review/business/domain/Topic.java
+++ src/peer/review/business/domain/Topic.java
@@ -1,5 +1,30 @@
 package peer.review.business.domain;
 
 public class Topic {
+	private String name;
 
+	public Topic(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
